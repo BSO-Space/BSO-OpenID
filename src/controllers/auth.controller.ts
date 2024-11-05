d95a1880ab6fb56ca5b004621c4d2f82c0cf1f83
boytur <104257779+boytur@users.noreply.github.com>
2024-11-05 21:47:19 +0700

    ✨ feat[auth.controller.ts]: Add redirect URL handling for authentication and update success redirection

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -48,7 +48,7 @@ export class AuthController {
     res: Response,
     next: NextFunction
   ): Promise<any> {
-    const { service } = req.query;
+    const { service, redirect } = req.query;
 
     if (!service) {
       return res.status(400).json({
@@ -58,7 +58,16 @@ export class AuthController {
       });
     }
 
+    if (!redirect) {
+      return res.status(400).json({
+        success: false,
+        message: "Redirect URL missing",
+        error: "Redirect URL is required to authenticate",
+      });
+    }
+
     req.session.service = service as string;
+
     req.session.save((err) => {
       if (err) {
         console.error("Failed to save session:", err);
@@ -66,7 +75,10 @@ export class AuthController {
       }
 
       passport.authenticate("discord", {
-        state: service as string,
+        state: JSON.stringify({
+          service: service as string,
+          redirect: redirect as string,
+        }),
       })(req, res, next);
     });
   }
@@ -80,7 +92,7 @@ export class AuthController {
     next: NextFunction
   ): Promise<any> {
     try {
-      const service = req.query.state as string;
+      const { service, redirect } = JSON.parse(req.query.state as string);
 
       if (!service) {
         return res.status(400).json({
@@ -120,7 +132,7 @@ export class AuthController {
         req.get("User-Agent") || ""
       );
 
-      res.redirect(`/auth/success?service=${service}`);
+      res.redirect(`/auth/success?service=${service}&redirect=${redirect}`);
     } catch (error) {
       next(error);
     }
@@ -135,12 +147,21 @@ export class AuthController {
     next: NextFunction
   ): Promise<any> {
     const serviceName = req.query.service as string;
+    const redirect = req.query.redirect as string;
 
     if (!serviceName) {
       return res.status(400).json({
         success: false,
         message: "Service information missing",
         error: "Service information is required to authenticate",
+      });
+    }
+
+    if (!redirect) {
+      return res.status(400).json({
+        success: false,
+        message: "Redirect URL missing",
+        error: "Redirect URL is required to authenticate",
       });
     }
 
@@ -152,7 +173,10 @@ export class AuthController {
       }
 
       passport.authenticate("github", {
-        state: serviceName,
+        state: JSON.stringify({
+          service: serviceName as string,
+          redirect: redirect as string,
+        }),
       })(req, res, next);
     });
   }
@@ -166,9 +190,9 @@ export class AuthController {
     next: NextFunction
   ): Promise<any> {
     try {
-      const serviceName = req.query.state as string;
+      const { service, redirect } = JSON.parse(req.query.state as string);
 
-      if (!serviceName) {
+      if (!service) {
         return res.status(400).json({
           success: false,
           message: "Service information missing",
@@ -176,21 +200,19 @@ export class AuthController {
         });
       }
 
-      const existingServices = await this.servicesService.findByNames(
-        serviceName
-      );
+      const existingServices = await this.servicesService.findByNames(service);
       if (!existingServices || !existingServices.public) {
         return res.status(403).json({
           success: false,
           message: "Service not found or disabled",
-          error: `Service '${serviceName}' is not available.`,
+          error: `Service '${service}' is not available.`,
         });
       }
 
       const userId = (req.user as { id: string }).id;
       const userHasService = await this.userService.userHasService(
         userId,
-        serviceName
+        service
       );
 
       if (!userHasService && existingServices.public) {
@@ -208,7 +230,7 @@ export class AuthController {
         req.get("User-Agent") || ""
       );
 
-      res.redirect(`/auth/success?service=${serviceName}`);
+      res.redirect(`/auth/success?service=${service}&redirect=${redirect}`);
     } catch (error) {
       next(error);
     }
@@ -220,6 +242,7 @@ export class AuthController {
   public async authSuccess(req: Request, res: Response): Promise<any> {
     try {
       const service = req.query.service as string;
+      const redirect = req.query.redirect as string;
 
       if (!req.isAuthenticated() || !service) {
         return res.status(401).json({
@@ -267,13 +290,7 @@ export class AuthController {
         req.get("User-Agent") || ""
       );
 
-      return res.json({
-        message: `Login successful for service ${service}`,
-        user: user,
-        service: service,
-        accessToken,
-        refreshToken,
-      });
+      return res.redirect(redirect || "/");
     } catch (error) {
       res.status(500).json({ error: error });
     }
