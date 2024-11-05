7a32d006a076c9fc96c90d18fb0c8208f393b2f4
boytur <104257779+boytur@users.noreply.github.com>
2024-11-05 19:32:01 +0700

    ✨ feat[app.ts]: Integrate CORS, cookie-parser, and rate limiting middleware for enhanced security and request management

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,36 @@ import auditMiddleware from "./middlewares/audit.middleware";
 import PassportConfig from "./config/passport.config";
 import AuthService from "./services/auth.service";
 import UserService from "./services/user.service";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(morgan("dev"));
+const corsOptions = {
+  origin: envConfig.APP_URL,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// request rate limiter
+const limiter = rateLimit({
+  windowMs: parseInt(envConfig.MAX_RATE_LIMIT_WINDOW_MS),
+  max: parseInt(envConfig.MAX_RATE_LIMIT_PER_IP),
+  message: {
+    success: false,
+    message: "Too many requests from this IP, please try again after some time",
+    error: "Your IP has been rate limited due to excessive requests",
+  },
+});
+
+app.use(limiter);
+
+app.use(morgan(`${envConfig.NODE_ENV === "production" ? "combined" : "dev"}`));
 
 app.use(
   session({
@@ -24,6 +50,8 @@ app.use(
 
 const authService = new AuthService();
 const userService = new UserService();
+
+// Initialize Passport
 new PassportConfig(authService, userService);
 
 app.use(passport.initialize());
