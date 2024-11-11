fae0b5411f5d80633876b57aa0af46ea64f4116b
boytur <104257779+boytur@users.noreply.github.com>
2024-11-11 21:15:12 +0700

    ✨ feat[hook.service.ts]: Implement login notification system with WebSocket support and detailed logging

--- src/services/hook.service.ts
+++ src/services/hook.service.ts
@@ -2,15 +2,40 @@ import { PrismaClient, User } from "@prisma/client";
 import { ServicesService } from "../services/service.service";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
+import { WebSocket } from "ws";
+import { webSocketClients } from "../server";
 
 const prisma = new PrismaClient();
 
+/**
+ * The HookService class.
+ * Contains methods for sending login notifications to services.
+ * @class - HookService
+ * @exports - HookService
+ * @constructor - Creates a new HookService object.
+ * @param servicesService - The ServicesService object.
+ * @method logHook - Logs the hook request to the database.
+ * @method generateSignature - Generates a signature using the payload and hook secret.
+ * @method sendLoginNotification - Sends a login notification to services.
+ *
+ */
 export class HookService {
   private servicesService: ServicesService;
 
   constructor(servicesService: ServicesService) {
     this.servicesService = servicesService;
   }
+
+  /**
+   * Logs the hook request to the database.
+   * @param serviceId - The ID of the service.
+   * @param url - The URL of the service.
+   * @param status  - The status of the request.
+   * @param statusCode - The status code of the request.
+   * @param requestBody - The request body.
+   * @param responseBody - The response body.
+   * @param errorMessage - The error message.
+   */
 
   private async logHook(
     serviceId: string,
@@ -21,6 +46,7 @@ export class HookService {
     responseBody?: any,
     errorMessage?: string
   ) {
+    // Log the hook request to the database
     await prisma.hookLog.create({
       data: {
         serviceId,
@@ -34,13 +60,29 @@ export class HookService {
     });
   }
 
-  // ฟังก์ชันสร้าง signature โดยใช้ HMAC-SHA256
+  /**
+   * Generates a signature using the payload and hook secret.
+   * @param payload - The payload to sign.
+   * @param hookSecret - The hook secret to use for signing.
+   * @returns The generated signature.
+   * @throws {Error} - If an error occurs while generating the signature.
+   */
+
   private generateSignature(payload: string, hookSecret: string): string {
     return crypto
       .createHmac("sha256", hookSecret)
       .update(payload)
       .digest("hex");
   }
+
+  /**
+   *  Sends a login notification to services.
+   * @param user - The user object.
+   * @param serviceName - The name of the service.
+   * @param ip - The IP address of the user.
+   * @param userAgent - The user agent of the user.
+   * @returns A boolean indicating whether the notification was sent successfully.
+   */
 
   public async sendLoginNotification(
     user: User,
@@ -49,39 +91,92 @@ export class HookService {
     userAgent: string
   ): Promise<boolean> {
     try {
+      // Find service by name
       const service = await this.servicesService.findByNames(serviceName);
+
+      // If service not found, throw an error
       if (!service) {
         throw new Error("Service not found.");
       }
 
+      // Generate JWT token with user ID, service name, IP, user agent, and timestamp
       const { hookSecret, microServicesUrl } = service;
-      const timestamp = new Date().toISOString();
 
-      // สร้าง JWT token พร้อม timestamp
+      // Generate JWT token with user ID, service name, IP, user agent, and timestamp
       const token = jwt.sign(
         {
           userId: user.id,
           service: serviceName,
           ip,
           userAgent,
-          timestamp,
         },
         hookSecret,
         { expiresIn: "5m" }
       );
 
+      // Create payload with event, user, service, IP, user agent, and timestamp
       const payload = JSON.stringify({
         event: "user.login",
         user,
         service: serviceName,
         ip,
         userAgent,
-        timestamp,
       });
 
-      // สร้าง signature ของ payload
+      // Generate signature using payload and hookSecret
       const signature = this.generateSignature(payload, hookSecret);
 
+      // Get WebSocket client from memory using clientId
+      const clientId = service.id;
+      const client = webSocketClients.get(clientId);
+
+      // If client is found, send login notification to WebSocket client
+      if (client) {
+        // Send login notification to WebSocket client
+        if (
+          client &&
+          client.readyState === WebSocket.OPEN &&
+          service.id === clientId
+        ) {
+          // Send login notification to WebSocket client
+          client.send(JSON.stringify({ payload, signature, token }));
+          console.log(
+            `[INFO] Sent login notification to client with ${service.name} 🚀`
+          );
+
+          // Log the WebSocket request
+          await this.logHook(service.id, "WebSocket", "success", 200, {
+            user,
+            service: serviceName,
+            ip,
+            userAgent,
+          });
+        } else {
+          // Log the error
+          console.log(
+            `[ERROR] Client with ID: ${clientId} is not connected or WebSocket is closed.`
+          );
+
+          await this.logHook(
+            service.id,
+            "WebSocket",
+            "failed",
+            500,
+            {
+              user,
+              service: serviceName,
+              ip,
+              userAgent,
+            },
+            null,
+            "Client is not connected or WebSocket is closed."
+          );
+
+          return false;
+        }
+      }
+
+      // Send login notification to microservices
       for (const url of microServicesUrl) {
         try {
           const response = await fetch(`${url}`, {
@@ -94,18 +189,19 @@ export class HookService {
             body: payload,
           });
 
+          // Log the response status
           const status = response.ok ? "success" : "failed";
           const statusCode = response.status;
 
-          // บันทึก log ของการส่ง hook
+          // Log the hook request
           await this.logHook(service.id, url, status, statusCode, {
             user,
             service: serviceName,
             ip,
             userAgent,
-            timestamp,
           });
 
+          // If response is not OK, log an error
           if (!response.ok) {
             console.error(
               `Failed to send notification to ${url}: HTTP status ${statusCode}`
@@ -113,16 +209,14 @@ export class HookService {
             return false;
           } else {
             console.log(`Login notification sent successfully to ${url}`);
-            return true;
           }
         } catch (error) {
-          // บันทึก log ในกรณีเกิดข้อผิดพลาด
           await this.logHook(
             service.id,
             url,
             "failed",
             500,
-            { userId: user.id, serviceName, ip, userAgent, timestamp },
+            { userId: user.id, serviceName, ip, userAgent },
             null,
             error instanceof Error ? error.message : String(error)
           );
@@ -130,7 +224,8 @@ export class HookService {
           console.error(`Failed to send notification to ${url}:`, error);
         }
       }
-      return false;
+
+      return true;
     } catch (error) {
       console.error("Failed to send login notification:", error);
       return false;
