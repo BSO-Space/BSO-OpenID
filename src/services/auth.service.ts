727f4aebe09f6ac4c483f6d235cbae942e8a422f
boytur <104257779+boytur@users.noreply.github.com>
2024-12-05 22:07:52 +0700

    ♻️ refactor[auth.service.ts]: Simplify code formatting and update user type to Partial<User>

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,34 +1,34 @@
-import jwt, { JwtPayload } from "jsonwebtoken";
-import { Account, AuditLog, PrismaClient, User } from "@prisma/client";
-import { readFileSync } from "fs";
-import path from "path";
-import { envConfig } from "../config/env.config";
-import logger from "../utils/logger.util";
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import { Account, AuditLog, PrismaClient, User } from '@prisma/client'
+import { readFileSync } from 'fs'
+import path from 'path'
+import { envConfig } from '../config/env.config'
+import logger from '../utils/logger.util'
 
 class AuthService {
-  private prisma: PrismaClient;
-  private privateAccessKey: Buffer;
-  private publicAccessKey: Buffer;
-  private privateRefreshKey: Buffer;
-  private publicRefreshKey: Buffer;
+  private prisma: PrismaClient
+  private privateAccessKey: Buffer
+  private publicAccessKey: Buffer
+  private privateRefreshKey: Buffer
+  private publicRefreshKey: Buffer
 
-  constructor() {
+  constructor () {
     // Create a new Prisma client
-    this.prisma = new PrismaClient();
+    this.prisma = new PrismaClient()
 
     // Read separate keys for access and refresh tokens
     this.privateAccessKey = readFileSync(
-      path.join(process.cwd(), "privateAccess.pem")
-    );
+      path.join(process.cwd(), 'privateAccess.pem')
+    )
     this.publicAccessKey = readFileSync(
-      path.join(process.cwd(), "publicAccess.pem")
-    );
+      path.join(process.cwd(), 'publicAccess.pem')
+    )
     this.privateRefreshKey = readFileSync(
-      path.join(process.cwd(), "privateRefresh.pem")
-    );
+      path.join(process.cwd(), 'privateRefresh.pem')
+    )
     this.publicRefreshKey = readFileSync(
-      path.join(process.cwd(), "publicRefresh.pem")
-    );
+      path.join(process.cwd(), 'publicRefresh.pem')
+    )
   }
 
   /**
@@ -37,18 +37,18 @@ class AuthService {
    * @returns The generated access token
    * @throws Error if the token cannot be generated
    */
-  public generateAccessToken(user: User, service: string): string {
+  public generateAccessToken (user: Partial<User>, service: string): string {
     return jwt.sign(
       {
         sub: user.id,
         name: user.username,
         iss: envConfig.APP_URL,
         iat: Math.floor(Date.now() / 1000),
-        service: service,
+        service: service
       },
       this.privateAccessKey,
-      { algorithm: "RS256", expiresIn: "1d" }
-    );
+      { algorithm: 'RS256', expiresIn: '1d' }
+    )
   }
 
   /**
@@ -57,18 +57,18 @@ class AuthService {
    * @returns The generated refresh token
    * @throws Error if the token cannot be generated
    */
-  public generateRefreshToken(user: User, service: string): string {
+  public generateRefreshToken (user: Partial<User>, service: string): string {
     return jwt.sign(
       {
         sub: user.id,
         name: user.username,
         iss: envConfig.APP_URL,
         iat: Math.floor(Date.now() / 1000),
-        service: service,
+        service: service
       },
       this.privateRefreshKey,
-      { algorithm: "RS256", expiresIn: "15d" }
-    );
+      { algorithm: 'RS256', expiresIn: '15d' }
+    )
   }
 
   /**
@@ -76,15 +76,15 @@ class AuthService {
    * @param token The token to verify
    * @returns The decoded payload if the token is valid, null otherwise
    */
-  public verifyAccessToken(token: string): JwtPayload | null {
+  public verifyAccessToken (token: string): JwtPayload | null {
     try {
       const decoded = jwt.verify(token, this.publicAccessKey, {
-        algorithms: ["RS256"],
-      }) as JwtPayload;
-      return decoded;
+        algorithms: ['RS256']
+      }) as JwtPayload
+      return decoded
     } catch (error) {
-      logger.error("Failed to verify access token:", error);
-      return null;
+      logger.error('Failed to verify access token:', error)
+      return null
     }
   }
 
@@ -93,15 +93,15 @@ class AuthService {
    * @param token The token to verify
    * @returns The decoded payload if the token is valid, null otherwise
    */
-  public verifyRefreshToken(token: string): JwtPayload | null {
+  public verifyRefreshToken (token: string): JwtPayload | null {
     try {
       const decoded = jwt.verify(token, this.publicRefreshKey, {
-        algorithms: ["RS256"],
-      }) as JwtPayload;
-      return decoded;
+        algorithms: ['RS256']
+      }) as JwtPayload
+      return decoded
     } catch (error) {
-      logger.error("Failed to verify refresh token:", error);
-      return null;
+      logger.error('Failed to verify refresh token:', error)
+      return null
     }
   }
 
@@ -111,32 +111,29 @@ class AuthService {
    * @returns The primary email of the user.
    * @throws Error if the email cannot be fetched.
    */
-  public async fetchGitHubEmails(accessToken: string): Promise<string> {
+  public async fetchGitHubEmails (accessToken: string): Promise<string> {
     try {
-      const response = await fetch("https://api.github.com/user/emails", {
+      const response = await fetch('https://api.github.com/user/emails', {
         headers: {
-          Authorization: `token ${accessToken}`,
-        },
-      });
+          Authorization: `token ${accessToken}`
+        }
+      })
 
       if (!response.ok) {
-        logger.error(
-          "Failed to fetch emails from GitHub:",
-          response.statusText
-        );
-        throw new Error("Unable to fetch GitHub emails");
+        logger.error('Failed to fetch emails from GitHub:', response.statusText)
+        throw new Error('Unable to fetch GitHub emails')
       }
 
-      const emails = await response.json();
-      const primaryEmail = emails.find((email: any) => email.primary)?.email;
+      const emails = await response.json()
+      const primaryEmail = emails.find((email: any) => email.primary)?.email
       if (!primaryEmail) {
-        throw new Error("Primary email not found for GitHub user");
+        throw new Error('Primary email not found for GitHub user')
       }
 
-      return primaryEmail;
+      return primaryEmail
     } catch (error) {
-      logger.error("Error fetching GitHub email:", error);
-      throw new Error("Failed to retrieve primary email from GitHub");
+      logger.error('Error fetching GitHub email:', error)
+      throw new Error('Failed to retrieve primary email from GitHub')
     }
   }
 
@@ -147,7 +144,7 @@ class AuthService {
    * @param ipAddress - The IP address of the request
    * @param userAgent - The user agent of the request
    */
-  public async logAudit(
+  public async logAudit (
     userId: string,
     action: string,
     ipAddress: string,
@@ -159,10 +156,10 @@ class AuthService {
         action,
         ipAddress,
         userAgent,
-        timestamp: new Date(),
-      },
-    });
+        timestamp: new Date()
+      }
+    })
   }
 }
 
-export default AuthService;
+export default AuthService
